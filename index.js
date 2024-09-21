f64d891ddc51e28b93a0124183a63ae8ea1a48be
Mehardeep Kaur <mehardeepk.38@gmail.com>
2024-09-21 15:46:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const express = require("express")();
-const http = require('http').Server(express);
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://mehardeepk38:JzlByvi3SPJJQKNj@cluster0.afkg8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-const india = require('./models/model');
-//const { name } = require("ejs");
+const http = require("http");
+const express = require("express");
+const path = require('path');
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-async function insert(){
-    await india.create({
-        name:'Mehardeep Kaur',
-        rollno:'2210997287' 
+// WebSocket connection
+io.on('connection', (socket) => {
+    console.log("A user connected");
+
+    socket.on("msg", (message) => {
+        console.log("A New User Message: ", message);
+        // Emit the message to all connected clients
+        io.emit("msg", message); // Emit 'msg' instead of 'message' for consistency
     });
-}
-insert();
-http.listen(3000,()=>{
-    console.log("server is running ")
-})
+
+    socket.on('disconnect', () => {
+        console.log("A user disconnected");
+    });
+});
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.resolve('./public')));
+
+// Serve the index.html file on the root route
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve('./public/index.html'));
+});
+
+// Start the server on port 9000
+server.listen(9000, () => 
+    console.log('Server Started at Port 9000')
+);
